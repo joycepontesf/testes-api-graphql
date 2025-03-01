79cb26c7c5d4fad9047aaadb66398f23f9ae5a0b
Joyce Pontes <joycepontesf@gmail.com>
2025-03-01 13:16:00 -0300

    Método para obter token

---
+++ tests/auth.test.js
@@ -0,0 +1,20 @@
+const { spec } = require('pactum');
+
+it('deve obter token válido', async () => {
+    await spec()
+        .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withGraphQLQuery(`
+        mutation AuthUser($email: String, $password: String) {
+            authUser(email: $email, password: $password) {
+              success
+              token
+            }
+          }
+    `)
+        .withGraphQLVariables({
+            "email": "admin@admin.com",
+            "password": "admin123"
+        })
+        .expectStatus(200)
+        .expectJson('data.authUser.success', true)
+})
